62ea6f72a4983b2434b365f3c49f1e299cf96e8f
unknown <delkanoberutu8@gmail.com>
2025-12-20 01:04:27 +0700

    redeployyy

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use App\Http\Controllers\Api\NotificationController;
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'Laravel OK',
+        'time' => now()->toDateTimeString(),
+    ]);
+});
+
+
 Route::post('/admin/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
