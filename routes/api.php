6f08f15a0645f1fae9258fc125511045ad36366c
unknown <delkanoberutu8@gmail.com>
2025-12-19 23:34:20 +0700

    tesst

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use App\Http\Controllers\Api\NotificationController;
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/destinations', [DestinationController::class, 'index']);
+
 Route::post('/admin/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
